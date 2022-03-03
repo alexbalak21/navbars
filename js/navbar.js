8fdda08ff396c0123b722d3cd218c11a1860927f
alexbalak21 <alex.balak@outlook.com>
2022-03-03 09:50:07 +0100

    Uploading navbars

---
+++ js/navbar.js
@@ -0,0 +1,5 @@
+let menuBtn = document.getElementById("menu-icon");
+let menu = document.querySelector("nav ul");
+// menu.classList.toggle("open");
+
+menuBtn.addEventListener("click", () => menu.classList.toggle("open"));
