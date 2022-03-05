5684e0aa131de3de9218d316c5858190875555a2
alexbalak21 <alex.balak@outlook.com>
2022-03-05 14:40:19 +0100

    flex nav done

--- flex/script.js
+++ flex/script.js
@@ -1,1 +1,24 @@
-const navToggle = document.querySelector(".navbar-toggle");
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const navIcon = document.querySelector(".nav-toggle i");
+
+navToggle.addEventListener("click", () => {
+    if (navMenu.classList.contains("active")) navIcon.className = "fas fa-bars";
+    else navIcon.className = "fa-solid fa-xmark";
+    navMenu.classList.toggle("active");
+});
+
+const itemsWithSubmenu = document.querySelectorAll(".has-submenu");
+const submenus = document.querySelectorAll(".submenu");
+
+for (let item of itemsWithSubmenu) {
+    item.addEventListener("click", () => {
+        const selSubmenu = item.querySelector(".submenu");
+        let active = selSubmenu.classList.contains("submenu-active");
+        for (let submenu of submenus) {
+            submenu.classList.remove("submenu-active");
+        }
+        if (active) selSubmenu.classList.remove("submenu-active");
+        else selSubmenu.classList.add("submenu-active");
+    });
+}
