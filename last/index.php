cb29a79e1cea70c8f8ac7faba971f4b609a6f891
alexbalak21 <alex.balak@outlook.com>
2024-02-11 01:18:09 +0100

    adding MUI for testing

---
+++ last/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://kit.fontawesome.com/6502c70df0.js" crossorigin="anonymous"></script>
+    <title>Navbar Responsive whith Dropdown</title>
+</head>
+
+<body>
+    <header>
+        <a class="logo" href=""><img src="logo.png" /></a>
+        <input type="checkbox" id="nav-toggle" />
+        <label for="nav-toggle"><i id="nav-bars" class="fa-solid fa-bars"></i></label>
+        <nav>
+            <ul>
+                <li><a href="">LINK</a></li>
+                <li>
+                    <a href="">
+                        LINK
+                        <i class="fa-solid fa-caret-down"></i>
+                    </a>
+                    <ul>
+                        <li>
+                            <a href="">
+                                DOWN
+                                <i class="fa-solid fa-caret-right"></i>
+                            </a>
+                            <ul>
+                                <li><a href="">LINK 1</a></li>
+                                <li><a href="">LINK2</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="">
+                                DWON
+                                <i class="fa-solid fa-caret-right"></i>
+                            </a>
+                            <ul>
+                                <li><a href="">LINK 1</a></li>
+                                <li><a href="">LINK2</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="">
+                        LINK
+                        <i class="fa-solid fa-caret-down"></i>
+                    </a>
+                    <ul>
+                        <li><a href="">LINK</a></li>
+                    </ul>
+                </li>
+                <li><a href="">LINK</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+
+</html>
---
+++ mui/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="mui/material.min.css">
+    <script src="mui/material.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <title>MUI TEST</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+    <button>TEST</button>
+    <button class="mdl-button mdl-js-button mdl-button--raised">
+        Button
+    </button>
+
+</body>
+
+</html>
